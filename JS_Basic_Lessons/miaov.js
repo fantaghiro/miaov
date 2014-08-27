43caf7d021cf89998e3235a8357d64c43b1008b9
fantaghiro <happy.wangpei@gmail.com>
2014-08-27 11:15:40 +0800

    9-7.html updated (for optimization)

--- JS_Basic_Lessons/miaov.js
+++ JS_Basic_Lessons/miaov.js
@@ -30,4 +30,29 @@ function doMove ( obj, attr, dir, target, endFn ) {
 	}, 30);
 }
 
+function shake ( obj, attr, endFn ) {
+
+	var pos = parseInt( getStyle(obj, attr) );			// 有隐患的
+	
+	var arr = [];			// 20, -20, 18, -18 ..... 0
+	var num = 0;
+	var timer = null;
+		
+	for ( var i=20; i>0; i-=2 ) {
+		arr.push( i, -i );
+	}
+	arr.push(0);
+	
+	clearInterval( obj.shake );
+	obj.shake = setInterval(function (){
+		obj.style[attr] = pos + arr[num] + 'px';
+		num++;
+		if ( num === arr.length ) {
+			clearInterval( obj.shake );
+			endFn && endFn();
+
+		}
+	}, 50);
+}
+
 function getStyle ( obj, attr ) { return obj.currentStyle?obj.currentStyle[attr] : getComputedStyle( obj )[attr]; }
