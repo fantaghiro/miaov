890d273b51a949952722efad397496b60af48503
fantaghiro <happy.wangpei@gmail.com>
2014-08-31 11:54:16 +0800

    miaov.js replaced

--- JS_Basic_Lessons/miaov.js
+++ JS_Basic_Lessons/miaov.js
@@ -7,7 +7,7 @@ function doMove ( obj, attr, dir, target, endFn ) {
 	
 	obj.timer = setInterval(function () {
 		
-		var speed = parseInt(getStyle( obj, attr )) + dir;			// 步长
+		var speed = parseInt(getStyle( obj, attr )) + dir;
 		
 		if ( speed > target && dir > 0 ||  speed < target && dir < 0  ) {
 			speed = target;
@@ -17,24 +17,45 @@ function doMove ( obj, attr, dir, target, endFn ) {
 		
 		if ( speed == target ) {
 			clearInterval( obj.timer );
-			
-			/*
-			if ( endFn ) {
-				endFn();
-			}
-			*/
 			endFn && endFn();
-			
 		}
 		
 	}, 30);
 }
 
-function shake ( obj, attr, endFn ) {
+function opacity(obj, num, target, endFn) {
+	
+		num = getStyle(obj, 'opacity')*100 < target ? num : -num;
+		
+		clearInterval( obj.opacity );
+		
+		obj.opacity = setInterval(function () {
+			
+			var speed = parseInt(getStyle(obj, 'opacity')*100) + num;
+			
+			if ( speed > target && num > 0 || speed < target && num < 0 ) {
+				speed = target;
+			}
+			
+			obj.style.opacity = speed/100;
+			obj.style.filter = 'alpha(opacity='+ speed +')';
+			
+			if ( speed == target ) {
+				clearInterval( obj.opacity );
+				endFn && endFn();
+			}
 
+		}, 20);
+}
+
+function shake ( obj, attr, endFn ) {
+	
+	if ( obj.onOff ) { return; }
+	obj.onOff = true;
+	
 	var pos = parseInt( getStyle(obj, attr) );			// 有隐患的
 	
-	var arr = [];			// 20, -20, 18, -18 ..... 0
+	var arr = [];
 	var num = 0;
 	var timer = null;
 		
@@ -42,7 +63,7 @@ function shake ( obj, attr, endFn ) {
 		arr.push( i, -i );
 	}
 	arr.push(0);
-	
+		
 	clearInterval( obj.shake );
 	obj.shake = setInterval(function (){
 		obj.style[attr] = pos + arr[num] + 'px';
@@ -50,7 +71,7 @@ function shake ( obj, attr, endFn ) {
 		if ( num === arr.length ) {
 			clearInterval( obj.shake );
 			endFn && endFn();
-
+			obj.onOff = false;
 		}
 	}, 50);
 }
