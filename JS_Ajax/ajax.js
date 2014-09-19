988c17c9460421866659e6ae4fe5fb6db49bb490
fantaghiro <happy.wangpei@gmail.com>
2014-09-19 12:48:32 +0800

    1-5， 1-6 added

---
+++ JS_Ajax/ajax.js
@@ -0,0 +1,44 @@
+function ajax(conf){
+	var type = conf.type;
+	var url = conf.url;
+	var data = conf.data;
+	var success = conf.success;
+
+	if(!type) {
+		type == 'get';
+	}
+	if(!data){
+		data == '';
+	}
+	if(!success){
+		success == null;
+	}
+
+	var xhr = null;
+	
+	try{
+		xhr = new XMLHttpRequest();
+	} catch(e) {
+		xhr = new ActiveXObject('Microsoft.XMLHTTP');
+	}
+
+	if (type == 'get' || 'GET') {
+		url = url + '?' + data;
+		xhr.open(type, url, true);
+		xhr.send();
+	} else if(type == 'post' || 'POST') {
+		xhr.open(type, url, true);
+		xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+		xhr.send(data);
+	}
+
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4){
+			if(xhr.status == 200){
+				success && success(JSON.parse(xhr.responseText));
+			} else {
+				alert('出错了，Error：' + xhr.status);
+			}
+		}
+	}
+}
